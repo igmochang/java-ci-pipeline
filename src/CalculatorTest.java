bc8f7cf8371b508fc87152d671cd681dd1d22653
igmochang <ig.morales@estudiantec.cr>
2021-04-03 00:42:51 -0600

    [Update]

---
+++ src/CalculatorTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void sum() {
+        Calculator calc = new Calculator();
+        calc.sum(5.0,5);
+        calc.sum(-1.2,-4.5);
+    }
+
+    @Test
+    void res() {
+        Calculator calc = new Calculator();
+        calc.res(-1.2,-1.2);
+        calc.res(11,3.0);
+    }
+
+    @Test
+    void div() {
+        Calculator calc = new Calculator();
+        calc.div(-12,0);
+        calc.div(5,10);
+    }
+
+    @Test
+    void mult() {
+        Calculator calc = new Calculator();
+        calc.mult(5,5);
+        calc.mult(0,651551);
+    }
+}
