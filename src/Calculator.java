9d5f7a0219600e5698bd5880377d75f182118922
igmochang <ig.morales@estudiantec.cr>
2021-04-03 00:25:58 -0600

    [Added] Todo

---
+++ src/Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+
+    public double sum(double a, double b) {
+        double res = a + b;
+        return res;
+    }
+
+    public double res(double a, double b) {
+        double res = a - b;
+        return res;
+    }
+
+    public double div(double a, double b) {
+        double res = a / b;
+        return res;
+    }
+
+    public double mult(double a, double b) {
+        double res = a*b;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(1, 3));
+        System.out.println(calc.res(10, 3));
+        System.out.println(calc.mult(9, 9));
+        System.out.println(calc.div(-5, 0));
+    }
+}
